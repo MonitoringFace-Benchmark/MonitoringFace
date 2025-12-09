ad6594ffdd00c3635a9c529115730b10fdb1b33c
LennardEc <Ldreese@web.de>
2025-12-09 15:24:55 +0100

    Update

--- Infrastructure/DataTypes/Verification/OutputStructures/IntermediateList.py
+++ Infrastructure/DataTypes/Verification/OutputStructures/IntermediateList.py
@@ -1,8 +1,11 @@
-from typing import AnyStr
+from typing import AnyStr, List
 
 from Infrastructure.DataTypes.Verification.OutputStructures.Structures.OooVerdicts import OooVerdicts
 from Infrastructure.DataTypes.Verification.OutputStructures.Structures.PropositionList import PropositionList
+from Infrastructure.DataTypes.Verification.OutputStructures.Structures.PropositionTree import PropositionTree, PDTLeave, \
+    PDTComplementSet, PDTSet
 from Infrastructure.DataTypes.Verification.OutputStructures.Structures.Verdicts import Verdicts
+from Infrastructure.DataTypes.Verification.OutputStructures.SubTypes.Assignment import Assignment
 from Infrastructure.DataTypes.Verification.OutputStructures.SubTypes.Proposition import Proposition
 from Infrastructure.DataTypes.Verification.OutputStructures.SubTypes.ValueType import ValueType
 
@@ -26,6 +29,37 @@ class IntermediateList:
         return cls(verdicts.verdict, verdicts.variable_order)
 
     @classmethod
-    def from_PropositionTree(cls, proposition_list: PropositionList):
+    def from_PropositionList(cls, proposition_list: PropositionList):
         values = [[tp, [Proposition(proposition_list.prop_list[tp])]] for tp in sorted(proposition_list.tp_to_ts.keys())]
         return cls(values, proposition_list.variable_order)
+
+    def to_proposition_tree(self, new_variable_order: List[AnyStr]) -> PropositionTree:
+        def _assignment_list(value_):
+            if isinstance(value_[0], Assignment):
+                return True
+            return False
+
+        tree = PropositionTree(new_variable_order)
+
+        for (tp, vals) in self.values:
+            if _assignment_list(vals):
+                new_assignments = map(lambda val: val.retrieve_order(new_variable_order), vals)
+                set_complement_list = extract_sets(new_assignments, new_variable_order)
+
+
+            else:
+                tree.insert(tp, tp, PDTLeave(value=vals[0]).value)
+
+        return tree
+
+
+def extract_sets(new_assignment, order):
+    collect_vals = []
+    for i in range(0, len(order)):
+        vals = set([val[i] for val in new_assignment])
+        collect_vals.append((PDTSet(vals), PDTComplementSet(vals)))
+    return collect_vals
+
+
+if __name__ == "__main__":
+    print(extract_sets([(1, 2), (3, 4), (5, 6), (7, 8)], ["x", "y"]))
